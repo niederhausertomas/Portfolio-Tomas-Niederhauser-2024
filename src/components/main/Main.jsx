7d1ddf8fa186c2c05a67b83a1e93621f9ac0df2d
Tomás Niederhauser <78279996+niederhausertomas@users.noreply.github.com>
2024-11-02 11:07:13 +0100

    about me

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -30,10 +30,10 @@ const Main = () => {
 			</h2>
 			<div className='container' id="parrafoContainer">
 				<p id="parrafo" >
-				Hello! I’m Tomás, a passionate front-end developer. After a career as a lawyer, I discovered my true calling in programming and made the life-changing decision to dive into tech.
-
-				I’m skilled in C++, HTML, CSS, JavaScript, and React.js, having studied at institutions like the National Technological University of Argentina, CoderHouse, and Devrock School. I’ve also gained hands-on experience with WordPress/PHP, Vue.js, MySQL, and TypeScript, continually learning through tutorials and developer communities to stay current.
-
+				Hello! I’m Tomás, a passionate front-end developer. After a career as a lawyer, I discovered my true calling in programming and made the life-changing decision to dive into tech.<br/><br/>
+				
+				I’m skilled in C++, HTML, CSS, JavaScript, and React.js, having studied at institutions like the National Technological University of Argentina, CoderHouse, and Devrock School. I’ve also gained hands-on experience with WordPress/PHP, Vue.js, MySQL, and TypeScript, continually learning through tutorials and developer communities to stay current.<br/><br/>
+				
 				I thrive in collaborative environments, value open communication, and enjoy helping others succeed. My goal is to keep growing as a programmer and, one day, pass on my knowledge to others.
 				</p>
 			</div>
